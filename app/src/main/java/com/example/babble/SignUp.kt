f2c3b15aa7e9b5dbfff6915158d4939eb157c3b5
Shashank A M <shashankam007@gmail.com-github>
2022-06-14 23:15:01 +0530

    Entering users into database

--- app/src/main/java/com/example/babble/SignUp.kt
+++ app/src/main/java/com/example/babble/SignUp.kt
@@ -7,6 +7,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SignUp : AppCompatActivity() {
 
@@ -15,6 +17,7 @@ class SignUp : AppCompatActivity() {
     private lateinit var edtName: EditText
     private lateinit var btnSignUp: Button
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,27 +31,38 @@ class SignUp : AppCompatActivity() {
         btnSignUp=findViewById(R.id.btnSignUp)
 
         btnSignUp.setOnClickListener {
+            val name=edtName.text.toString()
             val email=edtEmail.text.toString()
             val password=edtPassword.text.toString()
 
-            signUp(email,password);
+            signUp(name,email,password);
         }
 
     }
 
-    private fun signUp(email:String,password:String){
+    private fun signUp(name:String, email:String,password:String){
         //creating new user
         mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
+                    addUserToDatabase(name,email,mAuth.currentUser?.uid!!)
 
                     val intent = Intent(this@SignUp,MainActivity::class.java)
+
                     startActivity(intent)
+                    finish()
+
                 } else {
                     Toast.makeText(this@SignUp,"Some error occurred",Toast.LENGTH_SHORT).show()
 
                 }
             }
     }
+private fun addUserToDatabase(name: String,email: String,uid: String){
+
+    mDbRef=FirebaseDatabase.getInstance().getReference()
+    mDbRef.child("user").child(uid).setValue(User(name,email,uid))
+}
+
 
 }
---
+++ app/src/main/java/com/example/babble/User.kt
@@ -0,0 +1,15 @@
+package com.example.babble
+
+class User {
+    var name: String? = null
+    var email: String? = null
+    var vid: String? = null
+
+    constructor() {}
+    constructor(name: String?,email: String?,vid: String?){
+        this.name=name
+        this.email=email
+        this.vid=vid
+    }
+
+}
