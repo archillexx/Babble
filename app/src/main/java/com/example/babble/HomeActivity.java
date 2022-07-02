d633e9792abae6c826258ecf52c9182e9770d5ae
Ashwin Suresh <archillexx@Xerene.local>
2022-07-03 00:16:33 +0530

    Rework of App

---
+++ app/src/main/java/com/example/babble/HomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.babble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        if(auth.getCurrentUser()==null){
+            startActivity(new Intent(HomeActivity.this,RegistrationActivity.class));
+        }
+    }
+}
